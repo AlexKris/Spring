66d16c220e65731598d6c5b132b04a3fea29cd34
gaoruiting <gaoruiting@fantaike.ai>
2020-06-10 17:12:09 +0800

    多源

--- spring-data/spring-data-datasource/src/main/java/com/spring/datasource/SpringDataDatasourceApplication.java
+++ spring-data/spring-data-datasource/src/main/java/com/spring/datasource/SpringDataDatasourceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -15,6 +16,8 @@ import java.sql.SQLException;
 public class SpringDataDatasourceApplication implements CommandLineRunner {
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataDatasourceApplication.class, args);
@@ -23,6 +26,7 @@ public class SpringDataDatasourceApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         showConnection();
+        showData();
     }
 
     private void showConnection() throws SQLException {
@@ -30,5 +34,9 @@ public class SpringDataDatasourceApplication implements CommandLineRunner {
         Connection conn = dataSource.getConnection();
         log.info(conn.toString());
         conn.close();
+    }
+
+    private void showData() {
+        jdbcTemplate.queryForList("select * from foo").forEach(row -> log.info(row.toString()));
     }
 }
---
+++ spring-data/spring-data-multi/src/test/java/com/spring/multi/SpringDataMultiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.multi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMultiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
