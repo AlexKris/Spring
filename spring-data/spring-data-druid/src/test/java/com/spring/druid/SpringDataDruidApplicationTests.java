d183284616249ece4867f8113d1210835e978bbb
gaoruiting <gaoruiting@fantaike.ai>
2020-06-11 20:36:16 +0800

    druid

---
+++ spring-data/spring-data-druid/src/test/java/com/spring/druid/SpringDataDruidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.druid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataDruidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
