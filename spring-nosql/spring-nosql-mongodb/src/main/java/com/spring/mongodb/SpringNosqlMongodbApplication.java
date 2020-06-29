8974381385dce74e719fca5ebcde4b9832dec266
gaoruiting <gaoruiting@fantaike.ai>
2020-06-29 15:47:38 +0800

    :construction: mongodb repository

--- spring-nosql/spring-nosql-mongodb/src/main/java/com/spring/mongodb/SpringNosqlMongodbApplication.java
+++ spring-nosql/spring-nosql-mongodb/src/main/java/com/spring/mongodb/SpringNosqlMongodbApplication.java
@@ -3,6 +3,7 @@ package com.spring.mongodb;
 import com.mongodb.client.result.UpdateResult;
 import com.spring.mongodb.converter.MoneyReadConverter;
 import com.spring.mongodb.entity.Coffee;
+import com.spring.mongodb.repository.CoffeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
@@ -12,10 +13,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -25,9 +29,12 @@ import static org.springframework.data.mongodb.core.query.Query.query;
 
 @SpringBootApplication
 @Slf4j
+@EnableMongoRepositories
 public class SpringNosqlMongodbApplication implements ApplicationRunner {
     @Autowired
     private MongoTemplate mongoTemplate;
+    @Autowired
+    private CoffeeRepository coffeeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringNosqlMongodbApplication.class, args);
@@ -40,6 +47,11 @@ public class SpringNosqlMongodbApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        operationWithTemplate();
+        operationWithRepository();
+    }
+
+    private void operationWithTemplate() throws InterruptedException {
         Coffee espresso = Coffee.builder()
                 .name("espresso")
                 .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
@@ -65,5 +77,35 @@ public class SpringNosqlMongodbApplication implements ApplicationRunner {
         log.info("Update Result: {}", updateOne);
 
         mongoTemplate.remove(updateOne);
+    }
+
+    private void operationWithRepository() throws InterruptedException {
+        Coffee espresso = Coffee.builder()
+                .name("espresso")
+                .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
+        Coffee latte = Coffee.builder()
+                .name("latte")
+                .price(Money.of(CurrencyUnit.of("CNY"), 30.0))
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
+
+        coffeeRepository.insert(Arrays.asList(espresso, latte));
+        coffeeRepository.findAll(Sort.by("name")).forEach(
+                coffee -> log.info("Saved Coffee {}", coffee)
+        );
+
+        Thread.sleep(1000);
+        latte.setPrice(Money.of(CurrencyUnit.of("CNY"), 35.0));
+        latte.setUpdateTime(new Date());
+        coffeeRepository.save(latte);
+        coffeeRepository.findByName("latte").forEach(
+                coffee -> log.info("Coffee {}", coffee)
+        );
+
+        coffeeRepository.deleteAll();
     }
 }
