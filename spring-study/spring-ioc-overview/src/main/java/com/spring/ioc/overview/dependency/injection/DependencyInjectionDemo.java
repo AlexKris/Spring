36380820f7b9e334d73746b09616239d4f00c1c0
gaoruiting <oi.yi@outlook.com>
2021-04-11 23:04:08 +0800

    🚧Spring IoC 容器 BeanFactory 和 ApplicationContext

--- spring-study/spring-ioc-overview/src/main/java/com/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ spring-study/spring-ioc-overview/src/main/java/com/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -1,8 +1,9 @@
 package com.spring.ioc.overview.dependency.injection;
 
 import com.spring.ioc.overview.repository.UsrRepository;
-import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 依赖注入
@@ -23,6 +24,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *     1、实时注入
  *     2、延时注入
  * </pre>
+ * Bean定义配置
+ * <pre>
+ *     1、基于 XML 文件
+ *     2、基于 Properties 文件
+ *     3、基于 Java 注解
+ *     4、基于 Java API
+ * </pre>
+ * IoC容器配置
+ * <pre>
+ *     1、基于 XML 文件
+ *     2、基于 Java 注解
+ *     3、基于 Java API
+ * </pre>
+ * 外部化属性配置：基于 Java 注解
  *
  * @author chris
  */
@@ -31,21 +46,39 @@ public class DependencyInjectionDemo {
     public static void main(String[] args) {
         // step 1 配置 XML 配置文件
         // step 2 启动 Spring 应用上下文
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+//        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
 
-        // 依赖来源一：自定义 Bean
-        UsrRepository usrRepository = beanFactory.getBean("usrRepository", UsrRepository.class);
+        // 依赖注入 依赖来源一 自定义 Bean
+        UsrRepository usrRepository = applicationContext.getBean("usrRepository", UsrRepository.class);
 
         System.out.println(usrRepository.getUsrList());
 
-        // 依赖注入
+        // 依赖注入 依赖来源二 内建依赖
         System.out.println(usrRepository.getBeanFactory()); // DefaultListableBeanFactory
-        System.out.println(usrRepository.getBeanFactory() == beanFactory); // false
+        System.out.println(usrRepository.getBeanFactory() == applicationContext); // false
+
+        System.out.println(usrRepository.getObjectFactory().getObject()); // ClassPathXmlApplicationContext
+        System.out.println(usrRepository.getObjectFactory().getObject() == applicationContext); // true
 
         // 依赖查找
 //        System.out.println(beanFactory.getBean(BeanFactory.class)); // No qualifying bean of type 'org.springframework.beans.factory.BeanFactory' available
 
-        System.out.println(usrRepository.getObjectFactory().getObject()); // ClassPathXmlApplicationContext
-        System.out.println(usrRepository.getObjectFactory().getObject() == beanFactory); // true
+        // 依赖注入 依赖来源三 容器内建 Bean
+        Environment environment = applicationContext.getBean(Environment.class);
+        System.out.println("environment = " + environment);
+
+        whoIsIoCContainer(applicationContext, usrRepository);
+    }
+
+    // BeanFactory 和 ApplicationContext 哪个才是 Spring IoC 容器
+    private static void whoIsIoCContainer(ApplicationContext applicationContext, UsrRepository usrRepository) {
+        System.out.println(usrRepository.getBeanFactory()); // DefaultListableBeanFactory
+        System.out.println(usrRepository.getBeanFactory() == applicationContext);
+
+        // ApplicationContext is BeanFactory
+
+        // ConfigurableApplicationContext extends ApplicationContext
+        // ConfigurableApplicationContext#getBeanFactory()
     }
 }
